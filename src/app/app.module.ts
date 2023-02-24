f447f00b58a18031e6f31524f43d03f8ab9e94af
ivanpaezh <ivanpaezh10@hotmail.com>
2023-02-23 22:30:32 -0500

    tienda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './barra/barra.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
